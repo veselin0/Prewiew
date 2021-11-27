3397424780e9ad6e9e8fec1dfbe06ff0378b810d
veselin0 <vesselin60@gmail.com>
2021-11-27 17:03:53 +0200

    StrictMode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import {Component, StrictMode} from 'react';
 import './App.css';
 
 const Header = () => {
@@ -40,7 +40,9 @@ class Field extends Component {
 function App() {
   return (
     <div className="App">
-        <Header />
+        <StrictMode>
+            <Header />
+        </StrictMode>
         <Field/>
         <Btn/>
     </div>
